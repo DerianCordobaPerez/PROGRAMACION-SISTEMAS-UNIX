8bab400a8270246e0d019180c66505ab0d9de0c9
DerianCordobaPerez <derianunanleon@gmail.com>
2021-06-16 13:37:18 -0600

    Encuentro #04

---
+++ Bancos de preguntas #01/src/com/pplam/Main.java
@@ -0,0 +1,13 @@
+package com.pplam;
+
+public class Main implements IPrueba {
+
+    public static void main(String[] args) {
+
+        // Primera Pregunta
+        System.out.println(new Main().saltar("45"));
+
+        // Segunda Pregunta
+
+    }
+}
