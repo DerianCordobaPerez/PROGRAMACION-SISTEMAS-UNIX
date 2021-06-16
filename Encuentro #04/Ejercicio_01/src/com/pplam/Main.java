46076bad8da8e815b65277d75ca2861a234d568e
DerianCordobaPerez <derianunanleon@gmail.com>
2021-06-16 16:21:58 -0600

    Encuentro #04

--- Encuentro #04/Ejercicio_01/src/com/pplam/Main.java
+++ Encuentro #04/Ejercicio_01/src/com/pplam/Main.java
@@ -1,29 +1,45 @@
 package com.pplam;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
 
 public class Main extends JFrame {
     private JButton boton = new JButton("Presioname");
+    private JLabel label = new JLabel("Ingrese un texto");
+
+    private JTextField text = new JTextField();
+    private JPanel mainPanel = new JPanel();
+
     public Main() {
-        this.mostrarboton();
+        this.panel();
         this.eventoBoton();
-       this. setVisible(true);
-       this.setSize(500, 300);
-       this.setTitle("Hola mundo");
+
+        this. setVisible(true);
+        this.setSize(500, 300);
+        this.setTitle("Hola mundo");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public void panel() {
+        this.mainPanel.setLayout(new GridLayout(4, 5));
+        mostrarCajaDialog();
+        mostrarboton();
+        this.setContentPane(this.mainPanel);
     }
 
     public void mostrarboton() {
-        this.add(this.boton);
+        this.mainPanel.add(this.boton);
+    }
+
+    public void mostrarCajaDialog() {
+        this.mainPanel.add(this.label);
+        this.mainPanel.add(this.text);
     }
 
     public void eventoBoton() {
-        this.boton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "El boton ha sido presionado");
-            }
+        this.boton.addActionListener(e -> {
+            String texto = this.text.getText();
+            JOptionPane.showMessageDialog(null, texto);
         });
     }
 
